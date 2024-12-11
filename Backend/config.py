ea5497e45773934fcf69f66ecbe448df48e8e603
merytpeters <merytpeters@gmail.com>
2024-12-11 13:15:04 +0100

    Database configuration setup and basic flask app setup

--- Backend/config.py
+++ Backend/config.py
@@ -1,1 +1,14 @@
+#!/usr/bin/env python3
 """Configurations Settings File"""
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+class Config:
+    """Configuration class"""
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv('SECRET_KEY')
