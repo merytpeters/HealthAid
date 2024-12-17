5a4a6683b3ae4b66aa033fb1e17fc1ec3549ed29
Afuah Sekyiwaa Adusei <afuahsekyiwaaadusei@gmail.com>
2024-12-17 17:09:57 +0000

    Card update

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import Card from './Components/Card'
 import Hero from './Components/Hero'
+import Bottle from './assets/Drug-Bottle.png'
 import './App.css'
 
 function App() {
@@ -7,6 +8,12 @@ function App() {
   return (
     <>
       <Hero />
+      <p className='ldn-txt'>About us</p>
+      <p className='ldn-txt1'>
+        Health Aid is a comprehensive personal health management app that aims to enhance health literacy, foster proactive<br/> health management and empowers users to have control of their health.
+      </p>
+      <p className='ldn-txt'>Features</p>
+      <img src={Bottle}/>
       <Card>
         <p>
         Health Symptoms Journal<br />
--- Frontend/src/Components/Card.jsx
+++ Frontend/src/Components/Card.jsx
@@ -1,6 +1,6 @@
-const Card = ({children}) => {
+const Card = ({children, size = '20px', round = '36px'}) => {
     return (
-        <section className="card">
+        <section className="card" style={{fontSize: size, borderRadius: round}}>
             {children}
         </section>
     )
