958934fa1c09481510330e2f737e6f5b56a3af9f
Isah Abdulsalam <Isahabdulsalam416@gmail.com>
2024-12-26 16:04:27 +0100

    init file

--- Backend/app/__init__.py
+++ Backend/app/__init__.py
@@ -7,10 +7,12 @@ from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
 from config import Config
 from app.db import db
+from app.models.users import User
 from app.routes.auth import auth_bp
 from app.routes.healthInventory_routes import inventory_bp
 from app.routes.pillReminder_routes import pill_reminder_bp
 from app.routes.first_aid_guide_routes import first_aid_bp
+from app.routes.medicalJournal_routes import medical_journal_bp
 
 # Initialize extensions
 migrate = Migrate()
@@ -34,5 +36,6 @@ def create_app():
     app.register_blueprint(inventory_bp, url_prefix='/inventory')
     app.register_blueprint(pill_reminder_bp, url_prefix='/pill_reminder')
     app.register_blueprint(first_aid_bp, url_prefix='/first_aid')
+    app.register_blueprint(medical_journal_bp, url_prefix='/medical_journal')
 
     return app
