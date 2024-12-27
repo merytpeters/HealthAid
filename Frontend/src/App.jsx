089078105a99cfa93701a0fc46419e7971c3142d
precious-appiah <appiahprecious99@gmail.com>
2024-12-27 20:17:12 +0000

    side nav

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,18 +1,18 @@
-
-import Hero from './Components/Hero'
-import { Login } from './Pages/Login'
-import {Signup} from "./Pages/Signup"
-import './App.css'
+import Hero from "./Components/Hero";
+import { SideNav } from "./Components/SideNav";
+import { Login } from "./Pages/Login";
+import { Signup } from "./Pages/Signup";
+import "./App.css";
 
 function App() {
-  
   return (
     <>
       {/* <Hero /> */}
-      <Login />
+      {/* <Login /> */}
       {/* <Signup /> */}
+      <SideNav />                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ Frontend/src/Components/SideNav.jsx
@@ -0,0 +1,15 @@
+import "../Styles/sidenav.css";
+export const SideNav = () => {
+  return (
+    <div className="nav-items">
+      <div className="menus">
+        <h2>Health Aid</h2>
+        <div className="menu">Symptom Checker</div>
+        <div className="menu">Symptom Checker</div>
+        <div className="menu">Symptom Checker</div>
+        <div className="menu">Symptom Checker</div>
+        <div className="menu">Symptom Checker</div>
+      </div>
+    </div>
+  );
+};
