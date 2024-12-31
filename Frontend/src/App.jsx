8c9e56d01f9c9c2153ff99c489da5395150e32a1
Afuah Sekyiwaa Adusei <afuahsekyiwaaadusei@gmail.com>
2024-12-31 09:08:02 +0000

    backend test, dashboard start

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { Login } from './Pages/Login';
 import {Signup} from "./Pages/Signup";
 import LandingPage from './Pages/LandingPage';
 import Journal  from './Pages/Journal';
+import Dashboard from './Pages/Dashboard';
 import Hero from './Components/Hero';
 import './App.css';
 
@@ -15,6 +16,7 @@ function App() {
         <Route index element= {<LandingPage />}/>
         <Route path='/' element={<NavLayouts />}>
           <Route path='/journal' element={<Journal />} />
+          <Route path='/dashboard' element={<Dashboard />} />
         </Route>
       </>      
     )
